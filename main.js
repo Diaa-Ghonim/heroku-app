882b8ca0f5cf1635430ead4ea098fb39d2a92f4d
Diaa <deiaa_x@yahoo.com>
2020-12-01 05:12:25 +0200

    sjsj

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 document.getElementById('get-kitty').addEventListener('click', (e) => {
-  fetch('http://localhost:8080/kitty')
+  fetch('https://dcb-heroku-app.herokuapp.com/kitty')
     .then(res => res.json())
     .then(kitty => console.log(kitty))
     .catch(console.log)
